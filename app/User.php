611a949691279320a90678534710c348ed59ada6
maki.sato <mkuro4601@gmail.com>
2020-05-31 12:41:17 +0000

    follow, unfollow

--- app/User.php
+++ app/User.php
@@ -31,4 +31,111 @@ class User extends Authenticatable
     {
         return $this->hasMany(Posts::class);
     }
+    
+    
+    
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
+    }
+    
+    public function follow($userId)
+    {
+        // 既にフォローしているかの確認
+        $exist = $this->is_following($userId);
+        // 相手が自分自身ではないかの確認
+        $its_me = $this->id == $userId;
+    
+        if ($exist || $its_me) {
+            // 既にフォローしていれば何もしない
+            return false;
+        } else {
+            // 未フォローであればフォローする
+            $this->followings()->attach($userId);
+            return true;
+        }
+    }
+    
+    public function unfollow($userId)
+    {
+        // 既にフォローしているかの確認
+        $exist = $this->is_following($userId);
+        // 相手が自分自身かどうかの確認
+        $its_me = $this->id == $userId;
+    
+        if ($exist && !$its_me) {
+            // 既にフォローしていればフォローを外す
+            $this->followings()->detach($userId);
+            return true;
+        } else {
+            // 未フォローであれば何もしない
+            return false;
+        }
+    }
+    
+    public function is_following($userId)
+    {
+        return $this->followings()->where('follow_id', $userId)->exists();
+    }
+    
+    public function feed_posts()
+    {
+        $follow_user_ids = $this->followings()->pluck('users.id')->toArray();
+        $follow_user_ids[] = $this->id;
+        return Post::whereIn('user_id', $follow_user_ids);
+    }
+    
+    public function favorites()
+    {
+        return $this->belongsToMany(Posts::class, 'user_favorite', 'user_id', 'post_id')->withTimestamps();
+    }
+
+    public function favorite_users()
+    {
+        return $this->belongsToMany(Posts::class, 'user_favorite', 'post_id', 'user_id')->withTimestamps();
+    }
+    
+    public function favorite($postId)
+    {
+        // 既にフォローしているかの確認
+        $exist = $this->is_favorites($postId);
+        // 相手が自分自身ではないかの確認
+        
+    
+        if ($exist) {
+            // 既にフォローしていれば何もしない
+            return false;
+        } else {
+            // 未フォローであればフォローする
+            $this->favorites()->attach($postId);
+            return true;
+        }
+    }
+    
+    public function unfavorite($postId)
+    {
+        // 既にフォローしているかの確認
+        $exist = $this->is_favorites($postId);
+        // 相手が自分自身かどうかの確認
+        
+    
+        if ($exist) {
+            // 既にフォローしていればフォローを外す
+            $this->favorites()->detach($postId);
+            return true;
+        } else {
+            // 未フォローであれば何もしない
+            return false;
+        }
+    }
+    
+    public function is_favorites($postId)
+    {
+        return $this->favorites()->where('post_id', $postId)->exists();
+    }
 }
